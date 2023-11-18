933f3f87ff6c0910c4aa71db2096cfb89cc3f00e
Matas Udris <matas@udris.lt>
2023-11-18 15:50:31 +0100

    update gitignore, add temporary testing changes

--- experiments/UNet2d/train_unet2d_param_window_normalized.py
+++ experiments/UNet2d/train_unet2d_param_window_normalized.py
@@ -15,11 +15,12 @@ from leap3d.train import train_model, train
 from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
 
 
-def train_unet2d_param_window_normalized(experiment_name='unet2d_param_window_5_normalized', window_size=5, window_step_size=5, *args, **kwargs):
+def train_unet2d_param_window_normalized(experiment_name='unet2d_param_window_5_normalized', window_size=5, window_step_size=5, max_epochs=1, *args, **kwargs):
     train(
         experiment_name=experiment_name,
         window_size=window_size,
         window_step_size=window_step_size,
+        max_epochs=max_epochs,
         *args,
         **kwargs
     )
