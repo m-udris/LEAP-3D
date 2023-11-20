b145967a6db5c71ed626820aef2052c73857406e
Matas Udris <matas@udris.lt>
2023-11-21 00:03:59 +0100

    Add 2 experiments to 2d

---
+++ experiments/UNet2d/train_unet2d_param_window_10_normalized_by_100.py
@@ -0,0 +1,39 @@
+import logging
+from pathlib import Path
+import sys
+
+import torch
+from torchvision import transforms
+
+from leap3d.callbacks import get_checkpoint_only_last_epoch_callback, LogR2ScoreOverTimePlotCallback
+from leap3d.dataset import ExtraParam, LEAP3DDataModule
+from leap3d.models import Architecture
+from leap3d.config import DATA_DIR, DATASET_DIR, MELTING_POINT, BASE_TEMPERATURE, MAX_LASER_POWER, MAX_LASER_RADIUS
+from leap3d.train import train_model, train
+from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
+
+
+def train_unet2d_param_window_normalized(experiment_name='unet2d_param_window_10_normalized_100_epochs', window_size=10, window_step_size=10, max_epochs=100, *args, **kwargs):
+    target_transform = transforms.Compose([
+        torch.tensor,
+        transforms.Lambda(lambda x: normalize_temperature_2d(x, 100, inplace=True))
+    ])
+
+    target_to_train_transform = transforms.Compose([
+        transforms.Lambda(lambda x: get_target_to_train_transform(BASE_TEMPERATURE, MELTING_POINT, 0, 100)(x, inplace=True))
+    ])
+
+    train(
+        experiment_name=experiment_name,
+        window_size=window_size,
+        window_step_size=window_step_size,
+        max_epochs=max_epochs,
+        target_transforms=target_transform,
+        target_to_train_transforms=target_to_train_transform,
+        *args,
+        **kwargs
+    )
+
+
+if __name__ == '__main__':
+    train_unet2d_param_window_normalized(dataset_dir=Path(sys.argv[1]))
---
+++ experiments/UNet2d/train_unet2d_param_window_normalized_by_100.py
@@ -0,0 +1,36 @@
+import logging
+from pathlib import Path
+import sys
+
+import torch
+from torchvision import transforms
+
+from leap3d.config import DATA_DIR, DATASET_DIR, MELTING_POINT, BASE_TEMPERATURE, MAX_LASER_POWER, MAX_LASER_RADIUS
+from leap3d.train import train_model, train
+from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
+
+
+def train_unet2d_param_window_normalized(experiment_name='unet2d_param_window_5_normalized_by_100', window_size=5, window_step_size=5, max_epochs=100, *args, **kwargs):
+    target_transform = transforms.Compose([
+        torch.tensor,
+        transforms.Lambda(lambda x: normalize_temperature_2d(x, 100, inplace=True))
+    ])
+
+    target_to_train_transform = transforms.Compose([
+        transforms.Lambda(lambda x: get_target_to_train_transform(BASE_TEMPERATURE, MELTING_POINT, 0, 100)(x, inplace=True))
+    ])
+
+    train(
+        experiment_name=experiment_name,
+        window_size=window_size,
+        window_step_size=window_step_size,
+        max_epochs=max_epochs,
+        target_transforms=target_transform,
+        target_to_train_transforms=target_to_train_transform,
+        *args,
+        **kwargs
+    )
+
+
+if __name__ == '__main__':
+    train_unet2d_param_window_normalized(dataset_dir=Path(sys.argv[1]))
