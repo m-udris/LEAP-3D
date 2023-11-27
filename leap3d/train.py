5dbcab0c74df725ad2358c01ff3cc6f58f8f8db5
Matas Udris <matas@udris.lt>
2023-11-27 22:15:26 +0100

    Change num samples to 20 for train temperature video Plot reccurent steps instead of teacher-forced

--- leap3d/train.py
+++ leap3d/train.py
@@ -150,7 +150,7 @@ def train(
     callbacks = [
         get_checkpoint_only_last_epoch_callback(checkpoint_filename),
         LogR2ScoreOverTimePlotCallback(steps=eval_steps, samples=eval_samples),
-        PlotTopLayerTemperatureCallback(scan_parameters, plot_dir, steps=10, samples=10)
+        PlotTopLayerTemperatureCallback(scan_parameters, plot_dir, steps=10, samples=20)
     ]
 
     trainer = train_model(model=model, datamodule=datamodule, logger=wandb_logger, callbacks=callbacks, **hparams)
