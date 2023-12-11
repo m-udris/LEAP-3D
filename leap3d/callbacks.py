a44e406fd64fb6d7dc8bb457f01ca5f65749eb61
Matas Udris <matas@udris.lt>
2023-12-11 13:40:26 +0100

    fix bug in error plot callback

--- leap3d/callbacks.py
+++ leap3d/callbacks.py
@@ -134,7 +134,9 @@ class PlotErrorOverTimeCallback(Callback):
 
             # e(t) = sum((gt[t,:,:]-pred[t,:,])**2) / mean_t(sum((gt[t,:,:])**2))
             relative_error = torch.sum((actual_next_x_temperature - next_x_temperature)**2) / torch.min(torch.mean(actual_next_x_temperature**2), 1e-6 * torch.ones_like(actual_next_x_temperature))
+            relative_error = relative_error.detach().cpu().numpy()
             absolute_error = torch.sum(torch.abs(actual_next_x_temperature - next_x_temperature)) / torch.min(torch.mean(actual_next_x_temperature), 1e-6 * torch.ones_like(actual_next_x_temperature))
+            absolute_error = absolute_error.detach().cpu().numpy()
 
             relative_error_sum += relative_error
             absolute_error_sum += absolute_error
