712d05915d7a17b4a811bf5bc1c6e159fb720d69
Matas Udris <matas@udris.lt>
2023-12-12 00:21:34 +0100

    r2 change to diff temp in callback

--- leap3d/callbacks.py
+++ leap3d/callbacks.py
@@ -142,7 +142,7 @@ class PlotErrorOverTimeCallback(Callback):
             absolute_error = torch.sum(torch.abs(actual_next_x_temperature - next_x_temperature)) / torch.max(torch.mean(actual_next_x_temperature), torch.tensor(1e-6))
             absolute_error = absolute_error.detach().cpu().numpy()
 
-            r2_score_value = r2_score(next_x_temperature, actual_next_x_temperature)
+            r2_score_value = r2_score(y_hat, y)
 
             relative_error_sum += relative_error
             absolute_error_sum += absolute_error
