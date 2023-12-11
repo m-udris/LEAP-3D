6aa6928f01e4c503675b438e117b385aa524ac18
Matas Udris <matas@udris.lt>
2023-12-11 13:48:11 +0100

    batch size

--- experiments/UNet2d/train_unet2d_p_w5_l1_mse.py
+++ experiments/UNet2d/train_unet2d_p_w5_l1_mse.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from leap3d.train import train
 
 
-def train_unet2d_param_window_normalized(experiment_name='unet2d_p_w5_l1+mse_loss', window_size=5, window_step_size=5, max_epochs=50, *args, **kwargs):
+def train_unet2d_param_window_normalized(experiment_name='unet2d_p_w5_l1+mse_loss2', window_size=5, window_step_size=5, max_epochs=50, *args, **kwargs):
     loss_function = lambda y_hat, y: 0.5 * nn.functional.l1_loss(y_hat, y) + 0.5 * nn.functional.mse_loss(y_hat, y)
     train(
         experiment_name=experiment_name,
@@ -15,7 +15,7 @@ def train_unet2d_param_window_normalized(experiment_name='unet2d_p_w5_l1+mse_los
         window_step_size=window_step_size,
         max_epochs=max_epochs,
         loss_function=loss_function,
-        # batch_size=32,
+        batch_size=32,
         *args,
         **kwargs
     )
