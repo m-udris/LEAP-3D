cf7fe94dcea69c18e31f3e89c20aabfd5bf19f6e
Matas Udris <matas@udris.lt>
2024-01-23 17:42:59 +0200

    Try to solve circular reference wandb error

--- leap3d/callbacks.py
+++ leap3d/callbacks.py
@@ -293,10 +293,10 @@ class Rollout2DUNetCallback(Callback):
                 previous_x_pred_value = x_pred_temperature
 
             relative_error_normalizer = np.mean(np.array(relative_error_normalizer_list))
-            relative_errors = [relative_error / relative_error_normalizer for relative_error in relative_errors]
+            relative_errors = [(relative_error / relative_error_normalizer).numpy() for relative_error in relative_errors]
 
             absolute_error_normalizer = np.mean(np.array(absolute_error_normalizer_list))
-            absolute_errors = [absolute_error / absolute_error_normalizer for absolute_error in absolute_errors]
+            absolute_errors = [(absolute_error / absolute_error_normalizer).numpy() for absolute_error in absolute_errors]
 
         log_plot(f"Temperature Diff R2 score for rollout, epoch {current_epoch}", "R2 Score", temperature_diff_r2_scores)
         log_plot(f"Temperature R2 score for rollout, epoch {current_epoch}", "R2 Score", temperature_r2_scores)
