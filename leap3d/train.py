62b05f23da0852079e90678813236a957444fd7d
Matas Udris <matas@udris.lt>
2024-01-23 15:29:37 +0200

    Reenable r2 over time callback

--- leap3d/train.py
+++ leap3d/train.py
@@ -187,7 +187,7 @@ def train(
     ]
     if not no_callbacks:
         callbacks += [
-            # LogR2ScoreOverTimePlotCallback(steps=eval_steps, samples=eval_samples),
+            LogR2ScoreOverTimePlotCallback(steps=eval_steps, samples=eval_samples),
             # PlotTopLayerTemperatureCallback(scan_parameters, plot_dir, steps=10, samples=20),
             # PlotErrorOverTimeCallback()
             Rollout2DUNetCallback(scan_parameters)
