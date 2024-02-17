19ee7d2d1b894ad1a4df8b0b10f45dc37168446c
Matas Udris <matas@udris.lt>
2024-02-18 00:34:25 +0100

    Fix bug in TemperatureAroundLaser channels

--- leap3d/datasets/channels.py
+++ leap3d/datasets/channels.py
@@ -78,7 +78,7 @@ class RoughTemperatureAroundLaser(Channel):
         self.is_3d = is_3d
 
     def get(self, scan_parameters=None, scan_results=None, timestep=None, *args, **kwargs):
-        return scan_results.get_interpolated_grid_around_laser(timestep, 16, 0.25, scan_parameters, False, self.is_3d)[1]
+        return [scan_results.get_interpolated_grid_around_laser(timestep, 16, 0.25, scan_parameters, False, self.is_3d)[1]]
 
 
 class TemperatureAroundLaser(Channel):
@@ -87,7 +87,7 @@ class TemperatureAroundLaser(Channel):
         self.is_3d = is_3d
 
     def get(self, scan_parameters=None, scan_results=None, timestep=None, *args, **kwargs):
-        return scan_results.get_interpolated_grid_around_laser(timestep, 16, 0.25, scan_parameters, True, self.is_3d)[1]
+        return [scan_results.get_interpolated_grid_around_laser(timestep, 16, 0.25, scan_parameters, True, self.is_3d)[1]]
 
 
 class ScanningAngle(Channel):
