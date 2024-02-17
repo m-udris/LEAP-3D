2401c248336df0109edbe46a68a00c01ffa2c0b8
Matas Udris <matas@udris.lt>
2024-02-18 00:09:55 +0100

    Fix channels bug in base dataset

--- leap3d/datasets/base_dataset.py
+++ leap3d/datasets/base_dataset.py
@@ -90,11 +90,11 @@ class LEAPDataModule(pl.LightningDataModule):
         self.input_shape = input_shape if is_3d else input_shape[:2]
         self.target_shape = target_shape if is_3d else target_shape[:2]
 
-        self.input_channels = [channel(self.is_3d) for channel in input_channels]
+        self.input_channels = [channel(is_3d=self.is_3d) for channel in input_channels]
         self.input_channel_len = sum([channel.channels for channel in self.input_channels])
-        self.extra_input_channels = [channel(self.is_3d) for channel in extra_input_channels]
+        self.extra_input_channels = [channel(is_3d=self.is_3d) for channel in extra_input_channels]
         self.extra_input_channel_len = sum([channel.channels for channel in self.extra_input_channels])
-        self.target_channels = [channel(self.is_3d) for channel in target_channels]
+        self.target_channels = [channel(is_3d=self.is_3d) for channel in target_channels]
         self.target_channel_len = sum([channel.channels for channel in self.target_channels])
 
         self.transforms = transforms
