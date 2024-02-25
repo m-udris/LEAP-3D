5395c2ecd3e9d1737b7bb3ff849f0ae9b1a21035
Matas Udris <matas@udris.lt>
2024-02-25 20:58:31 +0100

    Add distances to mlp interpolation datamodule Add unfinished interpolation mlp training script for data generation

---
+++ experiments/InterpolationMLP/train_l1.py
@@ -0,0 +1,111 @@
+import logging
+from pathlib import Path
+import sys
+from typing import List
+
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.loggers.logger import Logger
+from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+import wandb
+from leap3d.callbacks import LogR2ScoreOverTimePlotCallback, PlotErrorOverTimeCallback, PlotTopLayerTemperatureCallback, Rollout2DUNetCallback, get_checkpoint_only_last_epoch_callback
+
+from leap3d.datasets.channels import RoughTemperatureAroundLaser, TemperatureAroundLaser, ScanningAngle, LaserPower, LaserRadius
+from leap3d.datasets import MLPInterpolationDataModule
+from leap3d.models import Architecture, LEAP3D_UNet2D
+from leap3d.config import DATA_DIR, DATASET_DIR, PARAMS_FILEPATH, ROUGH_COORDS_FILEPATH, MAX_LASER_POWER, MAX_LASER_RADIUS, MELTING_POINT, BASE_TEMPERATURE, NUM_WORKERS, FORCE_PREPARE
+from leap3d.models.lightning import InterpolationUNet2D
+from leap3d.scanning.scan_parameters import ScanParameters
+from leap3d.train import train_model
+from leap3d.transforms import normalize_extra_param, normalize_temperature_2d, normalize_temperature_3d, scanning_angle_cos_transform, get_target_to_train_transform
+
+
+def train():
+    dataset_dir = Path(sys.argv[1]) if len(sys.argv) > 1 else DATASET_DIR / 'mlp_interpolation'
+
+    hparams = {
+        'batch_size': 128,
+        'lr': 1e-3,
+        'num_workers': NUM_WORKERS,
+        'max_epochs': 100,
+        'transforms': 'default',
+        'in_channels': 1,
+        'out_channels': 1,
+        'extra_params_number': 3,
+        'input_channels': [RoughTemperatureAroundLaser],
+        'target_channels': [TemperatureAroundLaser],
+        'extra_params': [ScanningAngle, LaserPower, LaserRadius],
+        'activation': torch.nn.LeakyReLU,
+        'tags': ['MLP', '2D', 'interpolation', 'l1_loss'],
+        'force_prepare': False,
+        'is_3d': False,
+        'padding_mode': 'replicate',
+        'loss_function': 'l1'
+    }
+
+    # start a new wandb run to track this script
+    wandb_config = {
+        # set the wandb project where this run will be logged
+        'project': 'leap2d',
+        # name of the run on wandb
+        'name': f'interpolation_mlp_2d_l1_loss_b{hparams["batch_size"]}',
+        # track hyperparameters and run metadata
+        'config': hparams
+    }
+
+    logging.basicConfig(level=logging.INFO)
+    # wandb_logger = WandbLogger(log_model="all", **wandb_config)
+
+    train_transforms = {
+        'input': transforms.Compose([
+            torch.tensor,
+            transforms.Lambda(lambda x: normalize_temperature_2d(x, melting_point=MELTING_POINT, base_temperature=BASE_TEMPERATURE, inplace=True))
+        ]),
+        'target': transforms.Compose([
+            torch.tensor,
+            transforms.Lambda(lambda x: normalize_temperature_2d(x, melting_point=MELTING_POINT, base_temperature=BASE_TEMPERATURE, inplace=True))
+        ]),
+        'extra_params': transforms.Compose([
+            torch.tensor,
+            transforms.Lambda(lambda x: scanning_angle_cos_transform(x, 0, inplace=True)),
+            transforms.Lambda(lambda x: normalize_extra_param(x, 1, 0, MAX_LASER_POWER, inplace=True)),
+            transforms.Lambda(lambda x: normalize_extra_param(x, 2, 0, MAX_LASER_RADIUS, inplace=True))
+        ])
+    }
+
+    inverse_transforms = {
+        'input': transforms.Compose([
+            torch.tensor,
+            transforms.Lambda(lambda x: normalize_temperature_2d(x, melting_point=MELTING_POINT, base_temperature=BASE_TEMPERATURE, inverse=True, inplace=True))
+        ]),
+        'target': transforms.Compose([
+            torch.tensor,
+            transforms.Lambda(lambda x: normalize_temperature_2d(x, melting_point=MELTING_POINT, base_temperature=BASE_TEMPERATURE, inverse=True, inplace=True))
+        ])
+    }
+
+    datamodule = MLPInterpolationDataModule(PARAMS_FILEPATH, ROUGH_COORDS_FILEPATH, DATA_DIR, dataset_dir,
+                    is_3d=False, batch_size=hparams['batch_size'],
+                    train_cases=18, test_cases=[18, 19],
+                    input_shape=[128,128], target_shape=[128,128],
+                    extra_input_channels=hparams['extra_params'], input_channels=hparams['input_channels'], target_channels=hparams['target_channels'],
+                    transforms=train_transforms, inverse_transforms=inverse_transforms,
+                    force_prepare=False, num_workers=NUM_WORKERS)
+
+    datamodule.prepare_data()
+    datamodule.setup()
+    # model = InterpolationUNet2D(**hparams)
+    # checkpoint_filename = wandb_config['name']
+    # callbacks = [
+    #     get_checkpoint_only_last_epoch_callback(checkpoint_filename, monitor='val_loss', mode='min')
+    # ]
+    # trainer = train_model(model=model, datamodule=datamodule, logger=wandb_logger, callbacks=callbacks, **hparams)
+
+    # trainer.test(model, datamodule=datamodule)
+
+    # wandb.finish()
+
+if __name__ == '__main__':
+    train()
--- leap3d/datasets/mlp_interpolation.py
+++ leap3d/datasets/mlp_interpolation.py
@@ -4,9 +4,11 @@ from typing import Callable, List, Dict
 import numpy as np
 import torch
 from torch.utils.data import random_split
+from leap3d.contour import get_melting_pool_contour_2d
 
 from leap3d.datasets import LEAPDataModule, LEAPDataset
 from leap3d.datasets.channels import RoughTemperatureAroundLaser, TemperatureAroundLaser, ScanningAngle, LaserPower, LaserRadius
+import shapely.geometry as sg
 
 
 class MLPInterpolationDataset(LEAPDataset):
@@ -21,6 +23,7 @@ class MLPInterpolationDataset(LEAPDataset):
         self.melting_pool = self.data_file['melting_pool']
         self.melting_pool_indices = self.data_file['melting_pool_indices']
         self.laser_data = self.data_file['laser_data']
+        self.target_distances_to_melting_pool = self.data_file['distances_to_melting_pool']
 
         self.melting_pool_transform = self.transforms.get('melting_pool', lambda x: x)
         self.laser_data_transform = self.transforms.get('laser_data', lambda x: x)
@@ -35,6 +38,7 @@ class MLPInterpolationDataset(LEAPDataset):
         target = self.target_transform(target)
 
         target_coordinates = self.target_coordinates[idx]
+        target_distances_to_melting_pool = self.target_distances_to_melting_pool[idx]
 
         melting_pool_idx_start, melting_pool_idx_end = self.melting_pool_indices[idx]
         melting_pool = self.melting_pool[int(melting_pool_idx_start):int(melting_pool_idx_end)]
@@ -43,7 +47,7 @@ class MLPInterpolationDataset(LEAPDataset):
         melting_pool = self.melting_pool_transform(melting_pool)
         laser_data = self.laser_data_transform(laser_data)
 
-        return input, extra_input, target, target_coordinates, melting_pool, laser_data
+        return input, extra_input, target, target_coordinates, melting_pool, laser_data, target_distances_to_melting_pool
 
 
 class MLPInterpolationDataModule(LEAPDataModule):
@@ -78,8 +82,8 @@ class MLPInterpolationDataModule(LEAPDataModule):
         )
         self.dataset_class = MLPInterpolationDataset
         self.melting_pool_offset = 0
-        self.melt_pool_shape = (8,)
-        self.rough_coordinates_size = 16
+        self.melt_pool_shape = (9,)
+        self.rough_coordinates_box_size = 32
         self.interpolated_coordinates_scale = 0.25
 
     def create_h5_datasets(self, h5py_file):
@@ -88,6 +92,7 @@ class MLPInterpolationDataModule(LEAPDataModule):
         datasets['melting_pool'] = self.create_h5_melting_pool_dataset(h5py_file)
         datasets['melting_pool_indices'] = self.create_h5_melting_pool_indices_dataset(h5py_file)
         datasets['laser_data'] = self.create_h5_laser_data_dataset(h5py_file)
+        datasets['distances_to_melting_pool'] = self.create_h5_distances_to_melting_pool_dataset(h5py_file)
 
         return datasets
 
@@ -98,7 +103,7 @@ class MLPInterpolationDataModule(LEAPDataModule):
         return h5py_file.create_dataset("target_coordinates", dset_shape, maxshape=dset_maxshape, chunks=True)
 
     def create_h5_melting_pool_dataset(self, h5py_file):
-        dset_shape = (1, 8)  # x, y, z, temperature, temperature gradient x, temperature gradient y, temperature gradient z, phase indicator
+        dset_shape = (1, *self.melt_pool_shape)  # x, y, z, temperature, temperature gradient x, temperature gradient y, temperature gradient z, phase indicator, distance_to_contour
         dset_maxshape = (None, *dset_shape[1:])
 
         return h5py_file.create_dataset("melting_pool", dset_shape, maxshape=dset_maxshape, chunks=True)
@@ -115,34 +120,69 @@ class MLPInterpolationDataModule(LEAPDataModule):
 
         return h5py_file.create_dataset("laser_data", dset_shape, maxshape=dset_maxshape, chunks=True)
 
+    def create_h5_distances_to_melting_pool_dataset(self, h5py_file):
+        dset_shape = (1, *self.target_shape)
+        dset_maxshape = (None, *dset_shape[1:])
+
+        return h5py_file.create_dataset("distances_to_melting_pool", dset_shape, maxshape=dset_maxshape, chunks=True)
+
     def get_data_generator_timestep(self, scan_results, scan_parameters, timestep):
         data = super().get_data_generator_timestep(scan_results, scan_parameters, timestep)
         data['target_coordinates'] = self.get_target_coordinates(scan_results, scan_parameters, timestep)
-        melting_pool, melting_pool_indices = self.get_melting_pool(scan_results, timestep)
+
+        contour_shape = self.get_melting_pool_contour(scan_results, scan_parameters, timestep)
+
+        melting_pool, melting_pool_indices = self.get_melting_pool(scan_results, scan_parameters, contour_shape, timestep)
         data['melting_pool'] = melting_pool
         data['melting_pool_indices'] = melting_pool_indices
         data['laser_data'] = self.get_laser_data(scan_results, timestep)
+
+        data['distances_to_melting_pool'] = self.get_target_distances_to_melting_pool_2d(scan_results, scan_parameters, contour_shape, timestep)
 
         return data
 
     def get_target_coordinates(self, scan_results, scan_parameters, timestep):
         laser_x, laser_y = scan_results.laser_data[timestep][:2]
-        bounds = scan_parameters.get_bounds_around_position(laser_x, laser_y, self.rough_coordinates_size)
+        bounds = scan_parameters.get_bounds_around_position(laser_x, laser_y, self.rough_coordinates_box_size)
         if self.is_3d:
             return np.array(bounds)
         return np.array(bounds[:4])
 
-    def get_melting_pool(self, scan_results, timestep):
+    def get_melting_pool_contour(self, scan_results, scan_parameters, timestep):
+        temperature_high_res = scan_results.get_melt_pool_temperature_grid(scan_parameters, timestep)
+        if self.is_3d:
+            raise NotImplementedError("3D not implemented yet")
+        else:
+            contours = get_melting_pool_contour_2d(temperature_high_res, top_k=1)[0]
+        if len(contours) == 0:
+            laser_position = scan_results.get_laser_coordinates_at_timestep(timestep)
+            return sg.Point(laser_position)
+
+        contour = np.array(contours[0])
+        # Rescale coordinates from unit distance into meters
+        contour = ((contour - 128) / 128) * 0.0012
+        return sg.Polygon(contour)
+
+
+    def get_melting_pool(self, scan_results, scan_parameters, contour_shape, timestep):
         melt_pool_data = scan_results.melt_pool[timestep]
         if melt_pool_data == [[]]:
             melt_pool_data = np.zeros((0, *self.melt_pool_shape))
-        else:
-            melt_pool_data = np.array(melt_pool_data)
+            melting_pool_indices = np.array([self.melting_pool_offset, self.melting_pool_offset])
+            return melt_pool_data, melting_pool_indices
 
-            # filter out points that are not in the last layer if 2D
-            if not self.is_3d:
-                max_z = np.max(melt_pool_data[:, 2])
-                melt_pool_data = melt_pool_data[melt_pool_data[:, 2] == max_z]
+        melt_pool_data = np.array(melt_pool_data)
+
+        # filter out points that are not in the last layer if 2D
+        if not self.is_3d:
+            max_z = np.max(melt_pool_data[:, 2])
+            melt_pool_data = melt_pool_data[melt_pool_data[:, 2] == max_z]
+
+        if isinstance(contour_shape, sg.Point):
+            distances = [contour_shape.distance(sg.Point(p[:2])) for p in melt_pool_data[:, :2]]
+        else:
+            distances = [np.abs(contour_shape.exterior.distance(sg.Point(p[:2]))) for p in melt_pool_data[:, :2]]
+        melt_pool_data = np.column_stack((melt_pool_data, distances))
 
         n_points = melt_pool_data.shape[0]
 
@@ -171,17 +211,15 @@ class MLPInterpolationDataModule(LEAPDataModule):
             dset[offset:] = data
         dset.flush()
 
-    # def setup(self, stage: str, split_ratio: float=0.8):
-    #     if stage == 'fit' or stage is None:
-    #         full_dataset_path = self.prepared_data_path / "dataset.hdf5"
-    #         full_dataset = MLPInterpolationDataset(full_dataset_path, transforms=self.transforms, inverse_transforms=self.inverse_transforms)
+    def get_target_distances_to_melting_pool_2d(self, scan_results, scan_parameters, contour_shape, timestep):
+        laser_x, laser_y = scan_results.get_laser_coordinates_at_timestep(timestep)
+        points = scan_parameters.get_coordinates_around_position(laser_x, laser_y,
+                                                                 self.rough_coordinates_box_size,
+                                                                 scale=self.interpolated_coordinates_scale,
+                                                                 is_3d=self.is_3d)
+        if isinstance(contour_shape, sg.Point):
+            distances = [contour_shape.distance(sg.Point(p[:2])) for p in points]
+        else:
+            distances = [np.abs(contour_shape.exterior.distance(sg.Point(p[:2]))) for p in points]
 
-    #         train_points_count = int(np.ceil(len(full_dataset) * split_ratio))
-    #         val_points_count = len(full_dataset) - train_points_count
-    #         self.train_dataset, self.val_dataset = random_split(
-    #             full_dataset, [train_points_count, val_points_count], generator=torch.Generator().manual_seed(42)
-    #         )
-
-    #     if stage == 'test' or stage is None:
-    #         test_dataset_path = self.prepared_data_path / "test_dataset.hdf5"
-    #         self.test_dataset = MLPInterpolationDataset(test_dataset_path, transforms=self.transforms, inverse_transforms=self.inverse_transforms)
+        return np.array(distances).reshape(self.target_shape)
