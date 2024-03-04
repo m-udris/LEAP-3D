Change melting point saving in interpolation mlp dataloader to fixed size Allow initialized channels for datamodules Implement offset box around laser bound calculation Change interpolation method to interpolate points inside linearly and use nearest for points outside

leap3d/datasets/base_dataset.py
@@ -10,7 +10,7 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import random_split, DataLoader, Dataset
 
-from leap3d.datasets.channels import LaserPosition, RoughTemperature, RoughTemperatureDifference, ScanningAngle, LaserPower, LaserRadius
+from leap3d.datasets.channels import Channel, LaserPosition, RoughTemperature, RoughTemperatureDifference, ScanningAngle, LaserPower, LaserRadius
 from leap3d.scanning import ScanParameters, ScanResults
 
 
@@ -92,11 +92,16 @@ class LEAPDataModule(pl.LightningDataModule):
         self.input_shape = input_shape if is_3d else input_shape[:2]
         self.target_shape = target_shape if is_3d else target_shape[:2]
 
-        self.input_channels = [channel(is_3d=self.is_3d) for channel in input_channels]
+        if not any([isinstance(channel, Channel) for channel in input_channels]):
+            self.input_channels = [channel(is_3d=self.is_3d) for channel in input_channels]
         self.input_channel_len = sum([channel.channels for channel in self.input_channels])
-        self.extra_input_channels = [channel(is_3d=self.is_3d) for channel in extra_input_channels]
+
+        if not any([isinstance(channel, Channel) for channel in extra_input_channels]):
+            self.extra_input_channels = [channel(is_3d=self.is_3d) for channel in extra_input_channels]
         self.extra_input_channel_len = sum([channel.channels for channel in self.extra_input_channels])
-        self.target_channels = [channel(is_3d=self.is_3d) for channel in target_channels]
+
+        if not any([isinstance(channel, Channel) for channel in target_channels]):
+            self.target_channels = [channel(is_3d=self.is_3d) for channel in target_channels]
         self.target_channel_len = sum([channel.channels for channel in self.target_channels])
 
         self.transforms = transforms
@@ -105,9 +110,11 @@ class LEAPDataModule(pl.LightningDataModule):
         self.force_prepare = force_prepare
         self.num_workers = num_workers
 
-    def prepare_data(self):
-        self.prepare_train_data()
-        self.prepare_test_data()
+    def prepare_data(self, stage: str=None):
+        if stage == 'fit' or stage is None:
+            self.prepare_train_data()
+        if stage == 'test' or stage is None:
+            self.prepare_test_data()
 
     def check_if_prepared_data_exists(self, path):
         return path.exists()
@@ -138,6 +145,7 @@ class LEAPDataModule(pl.LightningDataModule):
             datasets = self.create_h5_datasets(f)
 
             for case_id, scan_result_filepath in scan_case_id_and_result_filepaths:
+                print(f"case id: {case_id}")
                 data_generator = self.get_data_generator(case_id, scan_result_filepath, params_file, rough_coordinates)
                 offset += self.write_data_to_h5(data_generator, datasets, offset)
 

leap3d/datasets/mlp_interpolation.py
@@ -82,6 +82,7 @@ class MLPInterpolationDataModule(LEAPDataModule):
                  transforms: Dict[str, Callable]={}, inverse_transforms: Dict[str, Callable]={},
                  include_melting_pool: bool=False,
                  include_distances_to_melting_pool: bool=False,
+                 melting_pool_points: int=5600,
                  force_prepare=False,
                  num_workers=0):
         super().__init__(
@@ -108,13 +109,14 @@ class MLPInterpolationDataModule(LEAPDataModule):
         self.interpolated_coordinates_scale = 0.25
         self.include_melting_pool = include_melting_pool
         self.include_distances_to_melting_pool = include_distances_to_melting_pool
+        self.melting_pool_points = melting_pool_points
 
     def create_h5_datasets(self, h5py_file):
         datasets = super().create_h5_datasets(h5py_file)
         datasets['target_coordinates'] = self.create_h5_target_coordinates_dataset(h5py_file)
         if self.include_melting_pool:
             datasets['melting_pool'] = self.create_h5_melting_pool_dataset(h5py_file)
-            datasets['melting_pool_indices'] = self.create_h5_melting_pool_indices_dataset(h5py_file)
+            # datasets['melting_pool_indices'] = self.create_h5_melting_pool_indices_dataset(h5py_file)
         datasets['laser_data'] = self.create_h5_laser_data_dataset(h5py_file)
         if self.include_distances_to_melting_pool:
             datasets['distances_to_melting_pool'] = self.create_h5_distances_to_melting_pool_dataset(h5py_file)
@@ -128,16 +130,11 @@ class MLPInterpolationDataModule(LEAPDataModule):
         return h5py_file.create_dataset("target_coordinates", dset_shape, maxshape=dset_maxshape, chunks=True)
 
     def create_h5_melting_pool_dataset(self, h5py_file):
-        dset_shape = (1, *self.melt_pool_shape)  # x, y, z, temperature, temperature gradient x, temperature gradient y, temperature gradient z, phase indicator, distance_to_contour
+        dset_shape = (1, self.melting_pool_points, *self.melt_pool_shape)  # x, y, z, temperature, temperature gradient x, temperature gradient y, temperature gradient z, phase indicator, distance_to_contour
         dset_maxshape = (None, *dset_shape[1:])
 
         return h5py_file.create_dataset("melting_pool", dset_shape, maxshape=dset_maxshape, chunks=True)
 
-    def create_h5_melting_pool_indices_dataset(self, h5py_file):
-        dset_shape = (1, 2)  # start index, end index
-        dset_maxshape = (None, 2)
-
-        return h5py_file.create_dataset("melting_pool_indices", dset_shape, maxshape=dset_maxshape, chunks=True)
 
     def create_h5_laser_data_dataset(self, h5py_file):
         dset_shape = (1, 7)  # x position, y position, laser power, laser radius, laser velocity x, laser velocity y, existence of melt pool
@@ -161,12 +158,10 @@ class MLPInterpolationDataModule(LEAPDataModule):
             data['distances_to_melting_pool'] = self.get_target_distances_to_melting_pool_2d(scan_results, scan_parameters, contour_shape, timestep)
 
         if self.include_melting_pool:
-            melting_pool, melting_pool_indices = self.get_melting_pool(scan_results, scan_parameters, contour_shape, timestep)
+            melting_pool = self.get_melting_pool(scan_results, scan_parameters, contour_shape, timestep)
             data['melting_pool'] = melting_pool
-            data['melting_pool_indices'] = melting_pool_indices
 
         data['laser_data'] = self.get_laser_data(scan_results, timestep)
-
 
         return data
 
@@ -194,11 +189,10 @@ class MLPInterpolationDataModule(LEAPDataModule):
 
 
     def get_melting_pool(self, scan_results, scan_parameters, contour_shape, timestep):
+        melt_pool_points = np.zeros((self.melting_pool_points, *self.melt_pool_shape))
         melt_pool_data = scan_results.melt_pool[timestep]
         if melt_pool_data == [[]]:
-            melt_pool_data = np.zeros((0, *self.melt_pool_shape))
-            melting_pool_indices = np.array([self.melting_pool_offset, self.melting_pool_offset])
-            return melt_pool_data, melting_pool_indices
+            return melt_pool_points
 
         melt_pool_data = np.array(melt_pool_data)
 
@@ -216,30 +210,15 @@ class MLPInterpolationDataModule(LEAPDataModule):
 
         n_points = melt_pool_data.shape[0]
 
-        melting_pool_indices = np.array([self.melting_pool_offset, self.melting_pool_offset + n_points])
-        self.melting_pool_offset += n_points
+        melt_pool_points[:n_points] = melt_pool_data
 
-        return melt_pool_data, melting_pool_indices
+        return melt_pool_points
 
     def get_laser_data(self, scan_results, timestep):
         laser_data = np.array(scan_results.laser_data[timestep])
 
         return laser_data
 
-    def write_to_h5_dataset(self, dset, data, offset):
-        if dset.name == '/melting_pool':
-            flattened_data = []
-            for item in data:
-                flattened_data.extend(item)
-            data = np.array(flattened_data)
-            dset.resize(self.melting_pool_offset + data.shape[0], axis=0)
-            dset[self.melting_pool_offset:] = data
-            self.melting_pool_offset += data.shape[0]
-        else:
-            data = np.array(data)
-            dset.resize(offset + data.shape[0], axis=0)
-            dset[offset:] = data
-        dset.flush()
 
     def get_target_distances_to_melting_pool_2d(self, scan_results, scan_parameters, contour_shape, timestep):
         laser_x, laser_y = scan_results.get_laser_coordinates_at_timestep(timestep)

leap3d/scanning/scan_parameters.py
@@ -120,13 +120,19 @@ class ScanParameters():
         return x_start, x_end, y_start, y_end, z_start, z_end
 
     def get_coordinates_around_position(self, x: float, y: float, size: int, scale: float=0.25, is_3d: bool=False):
-        x_start, x_end, y_start, y_end, z_start, z_end = self.get_bounds_around_position(x, y, size)
+        bounds = self.get_bounds_around_position(x, y, size)
 
-        x_points = np.linspace(x_start, x_end, int(size // scale))
-        y_points = np.linspace(y_start, y_end, int(size // scale))
+        return self.get_coordinates_within_bounds(bounds, size, scale, is_3d)
+
+    def get_coordinates_within_bounds(self,
+                                      bounds: Tuple[float, float, float, float, float, float],
+                                      size: int, scale: float=0.25, is_3d: bool=False):
+        x_min, x_max, y_min, y_max, z_min, z_max = bounds
+        x_points = np.linspace(x_min, x_max, int(size // scale))
+        y_points = np.linspace(y_min, y_max, int(size // scale))
 
         if is_3d:
-            z_points = np.linspace(z_start, z_end, int(len(self.rough_coordinates['z_rough'][0, 0, :]) // scale))
+            z_points = np.linspace(z_min, z_max, int(len(self.rough_coordinates['z_rough'][0, 0, :]) // scale))
         else:
             z_points = [np.max(self.rough_coordinates['z_rough'][0, 0, :])]
 
@@ -134,9 +140,45 @@ class ScanParameters():
 
         return points
 
+
+    def get_offset_bounds_around_laser(self, laser_data, size: int=24, offset_ratio: float=2/3):
+        laser_x, laser_y = laser_data[:2]
+        angle = self.scanning_angle
+        step_size = self.rough_coordinates_step_size
+        laser_velocity_x = laser_data[4]
+        direction = -np.sign(laser_velocity_x)
+
+        radius = size * step_size / 2
+        center_x = laser_x + offset_ratio * direction * radius * np.cos(angle)
+        center_y = laser_y + offset_ratio * direction * radius * np.sin(angle)
+
+        x_min, x_max, y_min, y_max, z_min, z_max = self.get_bounds_around_position(center_x, center_y, size)
+
+        scan_x_min = self.laser_x_min
+        scan_x_max = self.laser_x_max
+        scan_y_min = self.laser_y_min
+        scan_y_max = self.laser_y_max
+
+        oob_limit = radius * (1 - offset_ratio)
+        if x_min < scan_x_min - oob_limit:
+            x_max += scan_x_min - oob_limit - x_min
+            x_min = scan_x_min - oob_limit
+        if x_max > scan_x_max + oob_limit:
+            x_min -= x_max - scan_x_max - oob_limit
+            x_max = scan_x_max + oob_limit
+        if y_min < scan_y_min - oob_limit:
+            y_max += scan_y_min - oob_limit - y_min
+            y_min = scan_y_min - oob_limit
+        if y_max > scan_y_max + oob_limit:
+            y_min -= y_max - scan_y_max - oob_limit
+            y_max = scan_y_max + oob_limit
+
+        return x_min, x_max, y_min, y_max, z_min, z_max
+
+
     def __repr__(self):
         return f"""Case index:\t{self.case_index}
-Substrate Temperature:\t{self.substrate_temperature}
+Substrate Temperature:\t{self.substrate_temperature}1
 Scanning Angle (rad):\t{self.scanning_angle}
 Hatching Distance (m):\t{self.hatching_distance}
 Scanning Strategy:\t{self.scanning_strategy}"""

leap3d/scanning/scan_results.py
@@ -6,6 +6,16 @@ import scipy
 
 from leap3d.config import SCALE_DISTANCE_BY, SCALE_TIME_BY
 from leap3d.scanning import scan_parameters
+
+
+def custom_interpolate(points, values, desired_points):
+    desired_points = np.array(desired_points)
+    desired_values = interpolate.interpn(points, values, desired_points, method='linear', bounds_error=False, fill_value=np.nan)
+    nan_idxs = np.isnan(desired_values)
+    desired_values[nan_idxs] = interpolate.interpn(points, values, desired_points[nan_idxs], method='nearest', bounds_error=False, fill_value=None)
+    return desired_values
+
+
 class ScanResults():
     def __init__(self, results_filename: str | Path,
                  scale_distance_by: float=None,
@@ -67,10 +77,17 @@ class ScanResults():
         return self.laser_data[timestep][3]
 
     def get_melt_pool_coordinates_and_temperature(self, timestep: int):
+        if self.is_melt_pool_empty(timestep):
+            return [], []
+        melt_pool_at_timestep = np.array(self.melt_pool[timestep])
+        return melt_pool_at_timestep[:, :3], melt_pool_at_timestep[:, 3]
         coordinates = [point[:3] for point in self.melt_pool[timestep] if len(point) != 0]
         temperatures = [point[3] for point in self.melt_pool[timestep] if len(point) != 0]
 
         return coordinates, temperatures
+
+    def is_melt_pool_empty(self, timestep: int):
+        return len(self.melt_pool[timestep][0]) == 0
 
     def get_melt_pool_temperature_grid(self, scan_parameters, timestep: int):
         melt_pool_grid = np.zeros((256, 256, 64))
@@ -78,14 +95,20 @@ class ScanResults():
         if len(coordinates) == 0:
             return melt_pool_grid
         coord_delta = scan_parameters.rough_coordinates_step_size * 0.25
-
+        # new_coords = (coordinates / coord_delta + np.array([128, 128, 63])).astype(int)
         new_x_coords = [int((coord[0]) / coord_delta) + 128  for coord in coordinates]
         new_y_coords = [int((coord[1]) / coord_delta) + 128 for coord in coordinates]
         new_z_coords = [int((coord[2]) / coord_delta) + 63  for coord in coordinates]
 
+        # # melt_pool_grid2 = melt_pool_grid.copy()
+        # np.put(melt_pool_grid, new_coords, temperature, mode='raise')
+        # melt_pool_grid[new_coords[:, 0], new_coords[:, 1], new_coords[:, 2]] = temperature
         for i in range(len(coordinates)):
             melt_pool_grid[new_x_coords[i]][new_y_coords[i]][new_z_coords[i]] = temperature[i]
-
+        # if not np.all(melt_pool_grid == melt_pool_grid2):
+        #     print("\t".join([f"{x}" for x in melt_pool_grid.flatten()]))
+        #     print("\t".join([f"{x}" for x in melt_pool_grid2.flatten()]))
+        # assert np.all(melt_pool_grid == melt_pool_grid2)
         return melt_pool_grid
 
     def get_coordinate_points_and_temperature_at_timestep(self, scan_parameters, timestep: int, resolution: str=None):
@@ -195,7 +218,7 @@ class ScanResults():
         if include_high_resolution_points:
             high_res_points, high_res_temps = self.get_melt_pool_coordinates_and_temperature(timestep)
             if len(high_res_points) == 0:
-                interpolated_values = interpolate.interpn(grid_points, rough_temperature, points_to_interpolate, method='linear', bounds_error=False, fill_value=None)
+                interpolated_values = custom_interpolate(grid_points, rough_temperature, points_to_interpolate)
             else:
                 step_size = scan_parameters.rough_coordinates_step_size * step_scale
 
@@ -223,28 +246,32 @@ class ScanResults():
                 upscaled_z_range = np.arange(z_min, z_max + step_size, step_size)  # z coords are <= 0
 
                 upscaled_rough_coordinates = [(x, y, z) for x in upscaled_x_range for y in upscaled_y_range for z in upscaled_z_range]
-                upscaled_rough_temperatures = interpolate.interpn(grid_points, rough_temperature, upscaled_rough_coordinates, method='linear', bounds_error=False, fill_value=None)
+                upscaled_rough_temperatures = custom_interpolate(grid_points, rough_temperature, upscaled_rough_coordinates)
                 upscaled_rough_temperatures = upscaled_rough_temperatures.reshape(upscaled_x_range.shape[0], upscaled_y_range.shape[0], upscaled_z_range.shape[0])
 
-                for high_res_point, high_res_temp in zip(high_res_points, high_res_temps):
-                    x, y, z = high_res_point
-                    if upscaled_z_range[0] - z > step_size:
-                        continue
-                    x_diff = np.abs(upscaled_x_range - x)
-                    if np.min(x_diff) > step_size:
-                        continue
-                    x_index = np.argmin(x_diff)
-                    y_diff = np.abs(upscaled_y_range - y)
-                    if np.min(y_diff) > step_size:
-                        continue
-                    y_index = np.argmin(y_diff)
-                    z_index = np.argmin(np.abs(upscaled_z_range - z))
-                    upscaled_rough_temperatures[x_index, y_index, z_index] = high_res_temp
+                high_res_temps = high_res_temps.reshape((-1,1))
+                high_res_data = np.concatenate([high_res_points, high_res_temps], axis=-1)
+
+                new_x_min = upscaled_x_range[0]
+                new_x_max = upscaled_x_range[-1]
+
+                new_y_min = upscaled_y_range[0]
+                new_y_max = upscaled_y_range[-1]
+
+                new_z_min = upscaled_z_range[0]
+
+                high_res_data = np.array(list(filter(lambda x: (new_x_min <= x[0] <= new_x_max) and (new_y_min <= x[1] <= new_y_max) and (new_z_min <= x[2]), high_res_data)))
+
+                if high_res_data.shape[0] != 0:
+                    high_res_data[:, :3] = np.round((high_res_data[:, :3] - np.array([new_x_min, new_y_min, new_z_min])) / step_size)
+                    high_res_data[:, 2] = np.array([-1 if z >= upscaled_rough_temperatures.shape[2] else z for z in high_res_data[:, 2]]).astype(int)
+                    upscaled_rough_temperatures[high_res_data[:, 0].astype(int), high_res_data[:, 1].astype(int), high_res_data[:, 2].astype(int)] = np.max((high_res_data[:, 3], upscaled_rough_temperatures[high_res_data[:, 0].astype(int), high_res_data[:, 1].astype(int), high_res_data[:, 2].astype(int)]), axis=0)
 
                 upscaled_grid_points = [upscaled_x_range, upscaled_y_range, upscaled_z_range]
-                interpolated_values = interpolate.interpn(upscaled_grid_points, upscaled_rough_temperatures, points_to_interpolate, method='linear', bounds_error=False, fill_value=None)
+                interpolated_values = custom_interpolate(upscaled_grid_points, upscaled_rough_temperatures, points_to_interpolate)
+
         else:
-            interpolated_values = interpolate.interpn(grid_points, rough_temperature, points_to_interpolate, method='linear', bounds_error=False, fill_value=None)
+            interpolated_values = custom_interpolate(grid_points, rough_temperature, points_to_interpolate)
 
         points_to_interpolate = np.array(points_to_interpolate)
         edge_length = int(size // step_scale)
@@ -255,14 +282,3 @@ class ScanResults():
         interpolated_values = interpolated_values.reshape(shape)
 
         return (x_coords, y_coords, z_coords), interpolated_values
-
-    # def get_points_around_laser(self, timestep: int, size: int, scan_parameters: scan_parameters, include_high_res: bool=False, is_3d: bool=False):
-    #     # TODO: do we need this?
-    #     raise NotImplementedError
-    #     # Step 1. Interpolate low res to high res
-    #     # Step 2. if include high res, add high res points to low res grid
-    #     # Step 3. Get points around laser. Do this by masking the grid according to distance to laser in x and y axes, then remove padding
-
-    #     laser_x, laser_y = self.get_laser_coordinates_at_timestep(timestep)
-
-