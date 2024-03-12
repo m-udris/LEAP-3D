35eeb8ca52110687d61fd6b25522c6a85fba986c
Matas Udris <matas@udris.lt>
2024-03-12 11:33:28 +0100

    Increase batch size for int mlp grad training

--- experiments/InterpolationMLPChunks/train_l1_norm_coords_full_gradients.py
+++ experiments/InterpolationMLPChunks/train_l1_norm_coords_full_gradients.py
@@ -28,7 +28,7 @@ def train():
     dataset_dir = Path(sys.argv[1]) if len(sys.argv) > 1 else DATASET_DIR / 'mlp_interpolation_chunks_coordinates_gradients'
 
     hparams = {
-        'batch_size': 32,
+        'batch_size': 128,
         'lr': 1e-3,
         'num_workers': NUM_WORKERS,
         'max_epochs': 20,
