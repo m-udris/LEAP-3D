9bdbd828c960be508ff1a5579a361bf5d3e60cf1
Matas Udris <matas@udris.lt>
2024-03-30 11:00:12 +0200

    Use all cases for training

--- experiments/scripts/generate_mlp_chunk_data_gradients.py
+++ experiments/scripts/generate_mlp_chunk_data_gradients.py
@@ -60,9 +60,8 @@ def aggregate_datasets(path, cases, is_test=False):
 
 
 if __name__ == '__main__':
-    train_cases = list(range(40)) + list(range(100, 140))
-    test_cases = list(range(40, 44)) + list(range(140, 144))
-
+    train_cases = list(range(90)) + list(range(100, 190))
+    test_cases = list(range(90, 100)) + list(range(190, 200))
     generate_cases(DATASET_DIR / 'mlp_interpolation_chunks_gradients', train_cases + test_cases, force_prepare=True)
     aggregate_datasets(DATASET_DIR / 'mlp_interpolation_chunks_gradients', train_cases, is_test=False)
     aggregate_datasets(DATASET_DIR / 'mlp_interpolation_chunks_gradients', test_cases, is_test=True)
