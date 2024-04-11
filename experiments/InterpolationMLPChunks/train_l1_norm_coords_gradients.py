f5c341f91c95d31402cc9c894d65d1e42cc2f3ab
Matas Udris <matas@udris.lt>
2024-04-11 23:59:14 +0200

    Increase temporal grad weight

--- experiments/InterpolationMLPChunks/train_l1_norm_coords_gradients.py
+++ experiments/InterpolationMLPChunks/train_l1_norm_coords_gradients.py
@@ -72,7 +72,7 @@ def train():
         'activation': activation,
         'temperature_loss_weight': 1,
         'pos_grad_loss_weight': 1,
-        'temporal_grad_loss_weight': 2,
+        'temporal_grad_loss_weight': 3,
     }
 
     # start a new wandb run to track this script
