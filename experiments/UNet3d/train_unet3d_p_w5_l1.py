98bc6d12fb11874245589479ebd17f4ed519994d
Matas Udris <matas@udris.lt>
2024-05-29 17:17:44 +0200

    feat: update 3d forecasting unet training script

--- experiments/UNet3d/train_unet3d_p_w5_l1.py
+++ experiments/UNet3d/train_unet3d_p_w5_l1.py
@@ -7,7 +7,7 @@ from leap3d.train import train_model, train
 from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
 
 
-def train_unet3d_param_window_normalized(experiment_name='unet3d_p_w5_l1_loss_b32', window_size=5, window_step_size=5, max_epochs=50, *args, **kwargs):
+def train_unet3d_param_window_normalized(experiment_name='unet3d_p_w5_l1_loss', window_size=5, window_step_size=5, max_epochs=50, *args, **kwargs):
     train(
         experiment_name=experiment_name,
         project_name='leap3d',
@@ -17,6 +17,9 @@ def train_unet3d_param_window_normalized(experiment_name='unet3d_p_w5_l1_loss_b3
         loss_function='l1',
         is_3d=True,
         batch_size=16,
+        train_cases=list(range(0, 200, 2)),
+        test_cases=list(range(1, 200, 10)),
+        eval_cases=[5, 35, 65, 95, 105, 135, 165, 195],
         *args,
         **kwargs
     )
