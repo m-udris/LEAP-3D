049f5a065038ca4b2cb6b586cd57388cae7287d2
Matas Udris <matas@udris.lt>
2024-05-30 16:11:21 +0200

    fix: specify 3d unet archtecture in 3d forecating unet training

--- experiments/UNet3d/train_unet3d_p_w5_l1.py
+++ experiments/UNet3d/train_unet3d_p_w5_l1.py
@@ -5,7 +5,7 @@ import sys
 from leap3d.config import DATA_DIR, DATASET_DIR, MELTING_POINT, BASE_TEMPERATURE, MAX_LASER_POWER, MAX_LASER_RADIUS
 from leap3d.train import train_model, train
 from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
-
+from leap3d.models import Architecture
 
 def train_unet3d_param_window_normalized(experiment_name='unet3d_p_w5_l1_loss', window_size=5, window_step_size=5, max_epochs=50, *args, **kwargs):
     train(
@@ -21,6 +21,7 @@ def train_unet3d_param_window_normalized(experiment_name='unet3d_p_w5_l1_loss', 
         test_cases=list(range(1, 200, 10)),
         eval_cases=[5, 35, 65, 95, 105, 135, 165, 195],
         depth=4,
+        architecture=Architecture.LEAP3D_UNET3D,
         *args,
         **kwargs
     )
