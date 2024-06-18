4a17d19c02505040bdaa328d5e682689f6dd2fd0
Matas Udris <matas@udris.lt>
2024-06-18 18:11:08 +0200

    edit: change activation to tanh for fc unet

--- experiments/UNet2d/train_unet2d_p_w5_l1_serpentine.py
+++ experiments/UNet2d/train_unet2d_p_w5_l1_serpentine.py
@@ -2,12 +2,14 @@ import logging
 from pathlib import Path
 import sys
 
+import torch
+
 from leap3d.config import DATA_DIR, DATASET_DIR, MELTING_POINT, BASE_TEMPERATURE, MAX_LASER_POWER, MAX_LASER_RADIUS
 from leap3d.train import train_model, train
 from leap3d.transforms import get_target_to_train_transform, normalize_extra_param, normalize_temperature_2d, scanning_angle_cos_transform
 
 
-def train_unet2d_param_window_normalized(experiment_name='unet2d_serpentine_w5-ws1', window_size=5, window_step_size=1, max_epochs=50, *args, **kwargs):
+def train_unet2d_param_window_normalized(experiment_name='unet2d_serpentine_w5-ws1_tanh', window_size=5, window_step_size=1, max_epochs=50, *args, **kwargs):
     train(
         experiment_name=experiment_name,
         window_size=window_size,
@@ -18,6 +20,7 @@ def train_unet2d_param_window_normalized(experiment_name='unet2d_serpentine_w5-w
         train_cases=list(range(0, 100, 2)),
         test_cases=list(range(1, 100, 10)),
         eval_cases=[5, 35, 65, 95],
+        activation=torch.nn.tanh
         *args,
         **kwargs
     )
